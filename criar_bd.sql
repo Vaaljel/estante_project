08ff3347b58a5dbfe19daf814ddd3ae3b93a6275
Tomás <114113569+ThatPlayerSniper@users.noreply.github.com>
2025-03-23 18:37:25 +0000

    Debuged DB

--- criar_bd.sql
+++ criar_bd.sql
@@ -1,156 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 23, 2025 at 07:32 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE DATABASE estantedb;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-use estantedb;
-
-
+--
+-- Database: `estantedb`
+--
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `utilizadores`
---
--- NOTA:
--- FOI USADO O AUTO_INCREMENT PARA GERAR AUTOMATICAMENTE UM ID
--- FOI USADO UM ENUM PARA SIMPLIFICIDADE 
-
-CREATE TABLE utilizadores(
-    id_utilizador INT AUTO_INCREMENTNOT NOT NULL,
-    nome VARCHAR(12) NOT NULL,
-    endereco VARCHAR(50) NOT NULL,
-    secretpass VARCHAR(20) NOT NULL,
-    cargo ENUM('cliente', 'moderador', 'administrador') DEFAULT 'cliente' NOT NULL,
-    estado ENUM('registado', 'pendente', 'negado') DEFAULT 'pendente' NOT NULL,
-    data_registo DATE NOT NULL DEFAULT CURRENT_DATE,
-    PRIMARY KEY(id_utilizador)
-);
-
-
- 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `apontamento`
+-- Table structure for table `apontamento`
 --
 
-
-CREATE TABLE apontamento(
-    id_apo INT AUTO_INCREMENTNOT NOT NULL,
-    id_utilizador INT NOT NULL,
-    id_disciplina INT NOT NULL,
-    titulo VARCHAR(30) NOT NULL,
-    caminho_arquivo VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    estado_apo ENUM('aprovado', 'pendente', 'negado') DEFAULT 'pendente' NOT NULL,
-    data_submissao DATE NOT NULL DEFAULT CURRENT_DATE,
-    PRIMARY KEY(id_apo),
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador),
-    FOREIGN KEY (id_disciplina) REFERENCES disciplina(id_disciplina)
-);
+CREATE TABLE `apontamento` (
+  `id_apo` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL,
+  `id_disciplina` int(11) NOT NULL,
+  `titulo` varchar(30) NOT NULL,
+  `caminho_arquivo` varchar(255) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `estado_apo` enum('aprovado','pendente','negado') NOT NULL DEFAULT 'pendente',
+  `data_submissao` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `Curso`
+-- Table structure for table `avalicao`
 --
 
-CREATE TABLE curso(
-    id_curso INT AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    codigo INTEGER NOT NULL,
-    PRIMARY KEY(id_curso)
-);
-
+CREATE TABLE `avalicao` (
+  `id_avalicao` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL,
+  `avalicao` text NOT NULL,
+  `data_avaliacao` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `disciplina`
+-- Table structure for table `comentario`
 --
 
-CREATE TABLE disciplina(
-    id_disciplina INT AUTO_INCREMENT NOT NULL,
-    id_curso INT NOT NULL,
-    nome VARCHAR NOT NULL,
-    codigo INTEGER NOT NULL,    
-    descricao VARCHAR NOT NULL, -- Talvez
-    PRIMARY KEY (id_disciplina),
-    FOREIGN KEY (id_curso) REFERENCES curso(id_curso)
-);
-
+CREATE TABLE `comentario` (
+  `id_comentario` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL,
+  `cometario` text DEFAULT NULL,
+  `data_comentario` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avalicao`
+-- Table structure for table `curso`
 --
 
-
-CREATE TABLE avalicao(
-    id_avalicao INT AUTO_INCREMENT NOT NULL,
-    id_utilizador INT NOT NULL,
-    avalicao TEXT NOT NULL,
-    data_avaliacao DATE NOT NULL DEFAULT CURRENT_DATE,
-    PRIMARY KEY(id_avalicao),
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador)
-);
-
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `codigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `comentario`
+-- Table structure for table `disciplina`
 --
 
-
-CREATE TABLE comentario(
-    id_comentario INT AUTO_INCREMENT NOT NULL,
-    id_utilizador INT NOT NULL,
-    cometario TEXT,    
-    data_comentario DATE NOT NULL DEFAULT CURRENT_DATE,
-    PRIMARY KEY (id_comentario),
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador)
-);
-
+CREATE TABLE `disciplina` (
+  `id_disciplina` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `codigo` int(11) NOT NULL,
+  `descricao` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `sugestao`
+-- Table structure for table `sugestao`
 --
 
-
-CREATE TABLE sugestao(
-    id_sugestao INT AUTO_INCREMENT NOT NULL,
-    id_utilizador INT NOT NULL, 
-    sugestao TEXT,    
-    data_sugestao DATE NOT NULL DEFAULT CURRENT_DATE,
-    PRIMARY KEY (id_sugestao),
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador)
-);
-
-
+CREATE TABLE `sugestao` (
+  `id_sugestao` int(11) NOT NULL,
+  `id_utilizador` int(11) NOT NULL,
+  `sugestao` text DEFAULT NULL,
+  `data_sugestao` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
--- ALTER TABLES
 
--- Alter da tabela `apontamento` para associar com a tabela `utilizadores` 
-ALTER TABLE apontamento 
-    ADD CONSTRAINT fk_utilizador 
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador);
+--
+-- Table structure for table `utilizadores`
+--
 
--- Alter da tabela `comentario` para associar com a tabela `utilizadores`
-ALTER TABLE comentario 
-    ADD CONSTRAINT fk_comentario_utilizador 
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador);
+CREATE TABLE `utilizadores` (
+  `id_utilizador` int(11) NOT NULL,
+  `nome` varchar(12) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `secretpass` varchar(20) NOT NULL,
+  `cargo` enum('cliente','moderador','administrador') NOT NULL DEFAULT 'cliente',
+  `estado` enum('registado','pendente','negado') NOT NULL DEFAULT 'pendente',
+  `data_registo` date NOT NULL DEFAULT curdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Alter da tabela `sugestao` para associar com a tabela `utilizadores`
-ALTER TABLE sugestao 
-    ADD CONSTRAINT fk_sugestao_utilizador 
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador);
+--
+-- Indexes for dumped tables
+--
 
--- Alter da tabela `avaliacao` para associar com a tabela `utilizadores`
-ALTER TABLE avaliacao 
-    ADD CONSTRAINT fk_avaliacao_utilizador 
-    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador);
+--
+-- Indexes for table `apontamento`
+--
+ALTER TABLE `apontamento`
+  ADD PRIMARY KEY (`id_apo`),
+  ADD KEY `id_disciplina` (`id_disciplina`),
+  ADD KEY `fk_utilizador` (`id_utilizador`);
+
+--
+-- Indexes for table `avalicao`
+--
+ALTER TABLE `avalicao`
+  ADD PRIMARY KEY (`id_avalicao`),
+  ADD KEY `fk_avaliacao_utilizador` (`id_utilizador`);
+
+--
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `fk_comentario_utilizador` (`id_utilizador`);
+
+--
+-- Indexes for table `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`);
+
+--
+-- Indexes for table `disciplina`
+--
+ALTER TABLE `disciplina`
+  ADD PRIMARY KEY (`id_disciplina`),
+  ADD KEY `id_curso` (`id_curso`);
+
+--
+-- Indexes for table `sugestao`
+--
+ALTER TABLE `sugestao`
+  ADD PRIMARY KEY (`id_sugestao`),
+  ADD KEY `fk_sugestao_utilizador` (`id_utilizador`);
+
+--
+-- Indexes for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  ADD PRIMARY KEY (`id_utilizador`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apontamento`
+--
+ALTER TABLE `apontamento`
+  MODIFY `id_apo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `avalicao`
+--
+ALTER TABLE `avalicao`
+  MODIFY `id_avalicao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `disciplina`
+--
+ALTER TABLE `disciplina`
+  MODIFY `id_disciplina` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sugestao`
+--
+ALTER TABLE `sugestao`
+  MODIFY `id_sugestao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `utilizadores`
+--
+ALTER TABLE `utilizadores`
+  MODIFY `id_utilizador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `apontamento`
+--
+ALTER TABLE `apontamento`
+  ADD CONSTRAINT `apontamento_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`),
+  ADD CONSTRAINT `apontamento_ibfk_2` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`id_disciplina`),
+  ADD CONSTRAINT `fk_utilizador` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`);
+
+--
+-- Constraints for table `avalicao`
+--
+ALTER TABLE `avalicao`
+  ADD CONSTRAINT `avalicao_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`),
+  ADD CONSTRAINT `fk_avaliacao_utilizador` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`);
+
+--
+-- Constraints for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`),
+  ADD CONSTRAINT `fk_comentario_utilizador` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`);
+
+--
+-- Constraints for table `disciplina`
+--
+ALTER TABLE `disciplina`
+  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`);
+
+--
+-- Constraints for table `sugestao`
+--
+ALTER TABLE `sugestao`
+  ADD CONSTRAINT `fk_sugestao_utilizador` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`),
+  ADD CONSTRAINT `sugestao_ibfk_1` FOREIGN KEY (`id_utilizador`) REFERENCES `utilizadores` (`id_utilizador`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
