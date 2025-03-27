dfcdacf930da373f227e95420380ef622cbce29e
DCerquas <150840102+DCerquas@users.noreply.github.com>
2025-03-27 19:03:03 +0000

    comite

---
+++ FicheirosTempDiogo/esperaConfirmacao.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".register-btn").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
