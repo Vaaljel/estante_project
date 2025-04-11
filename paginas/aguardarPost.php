6650040aae2aa420496d703c888ca8f6bf456b0e
DCerquas <150840102+DCerquas@users.noreply.github.com>
2025-04-11 19:36:08 +0100

    --

---
+++ paginas/aguardarPost.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Moderação de Post's</title>
+  <link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:wght@100..900&display=swap" rel="stylesheet"/>
+  <link rel="stylesheet" href="aguardarPost.css"/>
+</head>
+<body>
+ 
+
+
+
+
+  <div class="container">
+    <div class="alerta">
+      <p class="mensagem">
+        O seu post foi enviado para um dos nossos moderadores para ser analisada para prosseguir para a publicação.
+      </p>
+      <a href="feed.php" class="botao">Feed</a>
+    </div>
+    <img src="gato_exclamacao.png" alt="Gato moderador" class="gato-img">
+  </div>
+</body>
+</html>
