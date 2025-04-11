bc88d9fcae84c139dc1049bbf0c277c3f7eb47fd
DCerquas <150840102+DCerquas@users.noreply.github.com>
2025-04-11 19:44:25 +0100

    Mini update do ultimo comite

--- paginas/aguardarPost.php
+++ paginas/aguardarPost.php
@@ -7,12 +7,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:wght@100..900&display=swap" rel="stylesheet"/>
   <link rel="stylesheet" href="aguardarPost.css"/>
 </head>
+
+<?php
+require_once './nav.php';
+require_once '../basedados/basedados.php';
+require_once '../basedados/auth.php';
+?>
+
 <body>
- 
-
-
-
-
   <div class="container">
     <div class="alerta">
       <p class="mensagem">
@@ -20,7 +22,7 @@
       </p>
       <a href="feed.php" class="botao">Feed</a>
     </div>
-    <img src="gato_exclamacao.png" alt="Gato moderador" class="gato-img">
+    <img src="../img/gato_exclamacao.png" alt="Gato moderador" class="gato-img">
   </div>
 </body>
 </html>
