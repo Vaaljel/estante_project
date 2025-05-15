9aa9e8da1b177e26eea9e756dddd0506e1bda8d3
Maria <mariaagamaa2005@gmail.com>
2025-05-15 09:58:07 +0100

    pagina de espera apos um registo, tentativa de resoluçaõ da sobreposição da imagem com a navbar

--- paginas/pagina_espera.php
+++ paginas/pagina_espera.php
@@ -24,17 +24,18 @@ if (session_status() == PHP_SESSION_NONE) {
     include './nav.php'
     ?>
 
-    <section class="container">
-        <img src="../img/gato_exclamacao.png" alt="Espera de Confirmação" class="waiting-image">
-        <div class="content">
-            <p>
-                Um pedido de confirmação foi enviado a um administrador para aprovar o seu registo.
-                Por favor, aguarde... Irá ser avisado por e-mail sobre o estado do pedido.
-            </p>
-            <button class="navbar-btn" onclick="window.location.href='index.php'">Página Principal</button>
-        </div>
-    </section>
-
+    <main class="main-content">
+        <section class="container">
+            <img src="../img/gato_exclamacao.png" alt="Espera de Confirmação" class="waiting-image">
+            <div class="content">
+                <p>
+                    Um pedido de confirmação foi enviado a um administrador para aprovar o seu registo.
+                    Por favor, aguarde... Irá ser avisado por e-mail sobre o estado do pedido.
+                </p>
+                <button class="navbar-btn" onclick="window.location.href='index.php'">Página Principal</button>
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
